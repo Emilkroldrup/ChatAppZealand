97371601adc559bf576abdf2b1ebf0a94c7faca2
Emilkr <Emilkroldrup@outlook.com>
2024-09-16 15:47:53 +0200

    start of helper classes

---
+++ chatapp/src/main/java/chatapp/domain/model/Message.java
@@ -0,0 +1,25 @@
+package chatapp.domain.model;
+
+public class Message {
+    private String content;
+    private String sender;
+
+    public Message(String content, String sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    @Override
+    public String toString() {
+        return sender + ": " + content;
+    }
+    
+}
