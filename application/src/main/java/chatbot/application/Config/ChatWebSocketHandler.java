8dd2323899be563e91c1f4f0d0d7fa5844023c1b
Emilkr <Emilkroldrup@outlook.com>
2024-09-23 21:40:42 +0200

    some style ;)

--- application/src/main/java/chatbot/application/Config/ChatWebSocketHandler.java
+++ application/src/main/java/chatbot/application/Config/ChatWebSocketHandler.java
@@ -1,24 +1,39 @@
 package chatbot.application.Config;
 
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("New connection: " + session.getId());
+        // Add session to the set
+        sessions.add(session);
+        System.out.println("Connected: " + session.getId());
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("Received message: " + message.getPayload());
-        session.sendMessage(new TextMessage("Server: " + message.getPayload()));
+        // Broadcast message to all clients
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                webSocketSession.sendMessage(new TextMessage("User " + session.getId() + ": " + message.getPayload()));
+            }
+        }
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
-        System.out.println("Connection closed: " + session.getId());
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        // Remove session from the set
+        sessions.remove(session);
+        System.out.println("Disconnected: " + session.getId());
     }
 }
