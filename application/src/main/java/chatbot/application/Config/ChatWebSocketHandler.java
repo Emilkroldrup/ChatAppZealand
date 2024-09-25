dc2b4a6ca9aa2056ee66b0d0aed6a52d52237770
Emilkr <Emilkroldrup@outlook.com>
2024-09-25 11:49:47 +0200

    multi user support

--- application/src/main/java/chatbot/application/Config/ChatWebSocketHandler.java
+++ application/src/main/java/chatbot/application/Config/ChatWebSocketHandler.java
@@ -1,39 +1,45 @@
 package chatbot.application.Config;
 
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
 public class ChatWebSocketHandler extends TextWebSocketHandler {
 
+    // Set to store all connected sessions
     private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        // Add session to the set
+        // Add new session to the set
         sessions.add(session);
-        System.out.println("Connected: " + session.getId());
+        System.out.println("Connected: " + session.getId() + " Total sessions: " + sessions.size());
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // Broadcast message to all clients
-        for (WebSocketSession webSocketSession : sessions) {
-            if (webSocketSession.isOpen()) {
-                webSocketSession.sendMessage(new TextMessage("User " + session.getId() + ": " + message.getPayload()));
+        // Broadcast the message to all connected clients
+        synchronized (sessions) {
+            System.out.println("Broadcasting message from session: " + session.getId() + " to " + sessions.size() + " clients");
+            for (WebSocketSession webSocketSession : sessions) {
+                if (webSocketSession.isOpen()) {
+                    System.out.println("Sending message to session: " + webSocketSession.getId());
+                    webSocketSession.sendMessage(new TextMessage("Client " + session.getId() + ": " + message.getPayload()));
+                } else {
+                    System.out.println("Session is closed: " + webSocketSession.getId());
+                }
             }
         }
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        // Remove session from the set
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        // Remove session when client disconnects
         sessions.remove(session);
-        System.out.println("Disconnected: " + session.getId());
+        System.out.println("Disconnected: " + session.getId() + " Remaining sessions: " + sessions.size());
     }
 }
